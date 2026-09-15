a1cc200211038bab3dc1f7941bca1b038d7f86e8
Bruno <0001240426@senaimgaluno.com.br>
2026-09-14 21:55:04 -0300

    Funcionalidade de salvar hotel

--- autenticar.php
+++ autenticar.php
@@ -5,14 +5,15 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 $sql = "SELECT * FROM clientes WHERE email = '$email' and senha = '$senha'";
+
 $resultado = mysqli_query($conexao, $sql);
 echo mysqli_num_rows($resultado);
 if(mysqli_num_rows($resultado) > 0){
-    //header("Location: minhas_reservas.php");
-    //exit();
+    header("Location: minhas_reservas.php");
+    exit();
 }else{
-    //header("Location: login.html");
-    //exit();
+    header("Location: login.html");
+    exit();
 }
 
 ?>
--- salvar_hotel.php
+++ salvar_hotel.php
@@ -1,1 +1,15 @@
-dsfgdsfsfdsfsafd
+<?php
+require_once "conexao.php";
+
+$hotel = $_POST['hotel'];
+$cidade = $_POST['cidade'];
+$estrelas = $_POST['estrelas'];
+
+$sql = "INSERT INTO clientes (hotel, cidade, estrelas)
+VALUES ('$hotel', '$cidade', $estrelas)";
+
+if(mysqli_query($conexao, $sql)){
+}
+else{
+}
+?>
