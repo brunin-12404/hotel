4bbddefa570af7a489b1c3586adb4fa0d083e9a8
Bruno <0001240426@senaimgaluno.com.br>
2026-09-16 22:09:21 -0300

    mexendo no site hotel

---
+++ autenticar_hotel.php
@@ -0,0 +1,19 @@
+<?php
+require_once "conexao.php";
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM hoteis WHERE email = '$email' and senha = '$senha'";
+
+$resultado = mysqli_query($conexao, $sql);
+echo mysqli_num_rows($resultado);
+if(mysqli_num_rows($resultado) > 0){
+    header("Location: minhas_reservas.php");
+    exit();
+}else{
+    header("Location: login_hotel.html");
+    exit();
+}
+
+?>
