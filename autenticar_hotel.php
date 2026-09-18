3098d961709f4aac149141ae8256c692fa0b5c6b
Bruno <0001240426@senaimgaluno.com.br>
2026-09-17 22:08:33 -0300

    Continuação do trabalho do site hotel

--- autenticar_hotel.php
+++ autenticar_hotel.php
@@ -9,11 +9,11 @@ $sql = "SELECT * FROM hoteis WHERE email = '$email' and senha = '$senha'";
 $resultado = mysqli_query($conexao, $sql);
 echo mysqli_num_rows($resultado);
 if(mysqli_num_rows($resultado) > 0){
-    header("Location: minhas_reservas.php");
+    header("Location: cadastar_quarto.php");
     exit();
 }else{
-    header("Location: login_hotel.html");
-    exit();
+    echo "<br> <br> Seu e-mail e senha não existem. ";
+    echo '<a href="login_hotel.html">Clique aqui para tentar novamente</a>';
 }
 
 ?>
